f1fce1239a9f259c7fde7b114914fc78728578bb
radyaardinp <radya.ardinp@gmail.com>
2025-11-20 23:09:04 +0700

    Update coba4.py

--- coba4.py
+++ coba4.py
@@ -478,6 +478,13 @@ if 'year' in df.columns and selected_pollutant in df.columns:
         # Warna line & fill
         PRIMARY = "#007bff"
         ACCENT = "#1f77b4"
+        def hex_to_rgba(hex_color, alpha=0.33):
+            hex_color = hex_color.lstrip('#')
+            r, g, b = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+            return f'rgba({r},{g},{b},{alpha})'
+        
+        fillcolor = hex_to_rgba(ACCENT, alpha=0.33)
+
         plot_df = df_filtered[["date", "PM2.5"]].dropna()
         fig = go.Figure()
         fig.add_trace(go.Scatter(
@@ -487,7 +494,7 @@ if 'year' in df.columns and selected_pollutant in df.columns:
             line=dict(color=PRIMARY, width=3),
             marker=dict(size=8),
             fill='tozeroy',
-            fillcolor=ACCENT + "55" 
+            fillcolor=fillcolor 
         ))
 
         fig.update_layout(
