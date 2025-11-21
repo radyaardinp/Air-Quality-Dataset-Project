41fddf6e7455d27b666bc996e43f02fd19fca44d
radyaardinp <radya.ardinp@gmail.com>
2025-11-21 20:07:49 +0700

    Update coba4.py

--- coba4.py
+++ coba4.py
@@ -443,16 +443,16 @@ if selected_station == "Semua Wilayah":
                     marker=dict(color=PEACH_PALETTE.get(stt, "#FF9E7A"))))
 
     fig.update_layout(
-        height=950,
-        width=None,
-        title="Tren Polutan 2013–2017",
-        margin=dict(l=20, r=20, t=80, b=40),   # kecilkan margin
         legend=dict(
-            orientation="h",
-            y=-0.25,     
-            x=0.5,
-            xanchor="center"),
-        template="plotly_white")
+            orientation="v",      # vertical
+            y=0.5,                # posisi tengah secara vertikal
+            x=1.02,               # sedikit di kanan plot
+            xanchor="left",
+            bgcolor="rgba(255,255,255,0.6)",  # background biar rapi
+            bordercolor="rgba(0,0,0,0.1)",
+            borderwidth=1,
+            font=dict(size=10)),
+        margin=dict(l=20, r=80, t=80, b=10))
 
     st.plotly_chart(fig, use_container_width=True)
 
