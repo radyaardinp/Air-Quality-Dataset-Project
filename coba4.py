16faffcf99e8b8a20021d2c5995dad297e80bbc1
radyaardinp <radya.ardinp@gmail.com>
2025-11-21 22:19:06 +0700

    Update coba4.py

--- coba4.py
+++ coba4.py
@@ -303,7 +303,7 @@ with col_b:
             names=cat_counts.index,
             values=cat_counts.values,
             color=cat_counts.index,
-            color_discrete_map=PEACH_PALETTE
+            color_discrete_map=PEACH_PALETTE,
             title=f"Proporsi Kategori Kualitas Udara<br><sup>Total: {len(df):,} data</sup>",
             hole=0)
     
